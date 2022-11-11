fa6b0501f9df187d48ef23c756d27a45f9826dae
PAMPACOMPASSO\lucas.r <lucas.r@compasso.com.br>
2022-11-11 13:48:30 -0300

    feat: crud booking

---
+++ src/screen/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {SimpleInput} from "../../components/SimpleInput";
+
+const Dashboard = () => {
+
+  return(
+    <React.Fragment>
+      <h1>dashboard</h1>
+      <SimpleInput type="text" />
+    </React.Fragment>
+
+  )
+}
+
+export default Dashboard;
